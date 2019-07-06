dd3c0c0210143d1d935db15df8b2788a270d6379
Sebastián Pineda Duque <spineda0424@gmail.com>
2019-07-06 11:11:29 -0500

    Se agregaron más dependencias, y se creó una estructura base de carpetas y archivos del Backend

--- src/index.js
+++ src/index.js
@@ -1,11 +1,31 @@
 const express = require("express")
-const app = express()
-const router = express.Router()
+const morgan = require("morgan")
+const path = require("path")
 
-app.listen(8000, () => {
-    console.log("El server ha sido levantado")
+// Inicializaciones
+const app = express()
+
+// Configuraciones
+
+// Middlewares
+app.use(morgan("dev"))
+app.use(express.urlencoded({extended: false})) // Para poder aceptar desde los formularios los datos que envíen los usuarios
+app.use(express.json()) // Para enviar y recibir JSON
+
+// Variables globales
+app.use((req, res, next) => {
+    next() // Toma el req, el res y el next continúa con el resto del código
 })
 
-app.get('/', (req, res) => {
-    res.send("Hola")
+// Rutas
+app.use(require("./rutas"))
+app.use(require("./rutas/autenticacion"))
+app.use("/links", require("./rutas/links"))
+
+// Public
+app.use(express.static(path.join(__dirname, "public")))
+
+// Iniciando el servidor
+app.listen(3000, () => {
+    console.log("Servidor encendido, en el puerto 3000")
 })
